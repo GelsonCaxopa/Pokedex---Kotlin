341bc748f86ee6928403908d9a72e59ecdc03ad8
Gelson Souza <gelson.majorit@gmail.com>
2022-08-10 06:48:57 -0300

    Finish App and ModelView

--- app/src/main/java/com/gelson/pokedex/view/MainActivity.kt
+++ app/src/main/java/com/gelson/pokedex/view/MainActivity.kt
@@ -2,7 +2,9 @@ package com.gelson.pokedex.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.gelson.pokedex.R
@@ -10,62 +12,34 @@ import com.gelson.pokedex.api.PokemonRepository
 import com.gelson.pokedex.api.model.PokemonResult
 import com.gelson.pokedex.domain.Pokemon
 import com.gelson.pokedex.domain.PokemonType
+import com.gelson.pokedex.viewmodel.PokemonViewModel
+import com.gelson.pokedex.viewmodel.PokemonViewModelFactory
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
+
+    private val viewModel by lazy{
+        ViewModelProvider(this, PokemonViewModelFactory())
+            .get(PokemonViewModel :: class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView = findViewById(R.id.rvPokemon)
-//        val charmander = Pokemon(
-//            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-//            1, "Charmander", listOf(PokemonType("Fire"))
-//        )
-//        val pokemons = listOf(
-//            charmander, charmander, charmander, charmander, charmander, charmander
-//        )
 
-        Thread(Runnable {
-            loadPokemons()
-
-        }).start()
-
-
-    }
-
-    private fun loadPokemons() {
-        val pokemonsApiResult = PokemonRepository.listPokemons()
-
-        pokemonsApiResult?.results?.let{
-
-            val pokemons : List<Pokemon?> = it.map {pokemonResult ->
-                val number = pokemonResult.url
-                    .replace("https://pokeapi.co/api/v2/pokemon/","")
-                    .replace("/","").toInt()
-                val pokemonApiResult =  PokemonRepository.getPokemon(number)
-
-                pokemonApiResult?.let {
-                    Pokemon(
-                        pokemonApiResult.id,
-                        pokemonApiResult.name,
-                        pokemonApiResult.types.map{ type ->
-                            type.type
-                        }
-                    )
-                }
-                }
-
-            val layoutManager = LinearLayoutManager(this)
-            recyclerView.post{
-                recyclerView.layoutManager = layoutManager
-                recyclerView.adapter = PokemonAdapter(pokemons)
-            }
-
-
-        }
+        viewModel.pokemons.observe(this, androidx.lifecycle.Observer {
+            loadRecyclerView(it)
+        })
 
     }
 
 
+    private fun loadRecyclerView(pokemons : List<Pokemon?>){
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            recyclerView.adapter = PokemonAdapter(pokemons)
+
+    }
 }
---
+++ app/src/main/java/com/gelson/pokedex/viewmodel/PokemonViewModelFactory.kt
@@ -0,0 +1,12 @@
+package com.gelson.pokedex.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+
+    class PokemonViewModelFactory : ViewModelProvider.Factory{
+
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return PokemonViewModel() as T
+        }
+    }
